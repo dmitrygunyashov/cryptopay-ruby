b4f482f6bc1c967e7b2216edccdde6580869d5a4
vadim <just.zimer@gmail.com>
2013-12-04 23:26:20 +0700

    ...

--- lib/cryptopay.rb
+++ lib/cryptopay.rb
@@ -1,5 +1,30 @@
 require "cryptopay/version"
+require 'rest_client'
+require 'net/http'
+require 'cryptopay/net'
 
 module Cryptopay
-  # Your code goes here...
+  # API Key
+  mattr_accessor :key
+
+  def self.new_invoice(params = {})
+    sanity_check!
+
+    Cryptopay::Request.post 'invoices', params
+  end
+
+  def self.invoice uuid
+    Cryptopay::Request.get "invoices/#{uuid}"
+  end
+
+  def self.configured?
+    self.key
+  end
+
+  def self.sanity_check!
+    unless configured?
+      raise Exception.new("Cryptopay Gem not properly configured")
+    end
+  end
+
 end
---
+++ lib/cryptopay/net.rb
@@ -0,0 +1,35 @@
+module Cryptopay
+  module Request
+    def self.to_uri(path)
+      "https://cryptopay.me/api/v1/#{path}"
+    end
+
+    def self.request(verb, path, options={})
+      verb = verb.upcase.to_sym
+
+      options[:api_key] = Cryptopay.key
+
+      resource = RestClient::Resource.new to_uri(path), content_type: 'application/json'
+      begin
+        if verb == :GET
+          response = RestClient.get "#{to_uri(path)}?".concat(options.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&'))
+        elsif verb == :POST
+          response = resource.post options
+        end
+
+        return JSON.parse response
+
+      rescue => e
+        raise Exception,JSON.parse(e.response)
+      end
+    end
+
+    def self.get(path, options={})
+      self.request(:GET, path, options)
+    end
+
+    def self.post(path, options={})
+      self.request(:POST, path, options)
+    end
+  end
+end
